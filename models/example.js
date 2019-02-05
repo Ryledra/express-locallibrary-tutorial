717ea8b40c5541bb681fc56d55f277fc35b0f47f
Ryledra <vince@nadin.one>
2019-02-05 21:33:39 +0000

    Schemas added to /models/ folder

---
+++ models/example.js
@@ -0,0 +1,22 @@
+// File: ./schemas/example.js
+
+//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+//var Schema = mongoose.Schema;
+
+var example_schema = new mongoose.Schema(
+    {
+      name: String,
+      binary: Buffer,
+      living: Boolean,
+      updated: { type: Date, default: Date.now() },
+      age: { type: Number, min: 18, max: 65, required: true },
+      mixed: Schema.Types.Mixed,
+      _someId: Schema.Types.ObjectId,
+      array: [],
+      ofString: [String], // You can also have an array of each of the other types too.
+      nested: { stuff: { type: String, lowercase: true, trim: true } }
+    }
+)
